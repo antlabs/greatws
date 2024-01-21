a0a462a3ad6f53e95801b668e589d0e4ee6fc744
guonaihong <guonaihong@qq.com>
2024-01-21 22:05:49 +0800

    新增freebsd 编译条件

--- api_kqueue.go
+++ api_kqueue.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build darwin
-// +build darwin
+//go:build darwin || freebsd
+// +build darwin freebsd
 
 package greatws
 
